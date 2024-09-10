14f31e084580213d7aa4af5f9c21fdf0e00d18fa
gustavofelipenascimento <gufenascimento@gmail.com>
2024-09-10 15:43:19 -0300

    tradetheme

--- src/contexts/TradeTheme.jsx
+++ src/contexts/TradeTheme.jsx
@@ -1,8 +1,22 @@
 import React, { useContext } from "react";
-import { ThemeContext } from "./ThemeContexts";
 import { Button } from "react-native-paper";
+import styles from "../config/styles";
+import { ThemeContext } from "./ThemeContexts";
 
 export default function TradeTheme() {
-    const { tradeTheme } = useContext(ThemeContext)
-    return <Button onPress={tradeTheme}>Trocar Tema</Button>
+  const { tradeTheme } = useContext(ThemeContext);
+
+  return (
+    <Button
+      mode="contained"
+      style={styles.button}
+      onPress={
+        tradeTheme ||
+        (() => console.warn("tradeTheme function is not available"))
+      }
+      accessibilityLabel="Toggle theme"
+    >
+      Trocar Tema
+    </Button>
+  );
 }
